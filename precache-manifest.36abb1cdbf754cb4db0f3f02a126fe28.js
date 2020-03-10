d4c12cab1c6f3f719bfd3842dac68d98fdbb5fc9
Eslam Mohamed <eslamm7amed123@gmail.com>
2020-03-10 17:25:28 +0200

    Updates

---
+++ precache-manifest.36abb1cdbf754cb4db0f3f02a126fe28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d65768f5cb16398614ea8109b9bb035",
+    "url": "/watch-it/index.html"
+  },
+  {
+    "revision": "25715e537e46f857e63a",
+    "url": "/watch-it/static/css/main.904cabae.chunk.css"
+  },
+  {
+    "revision": "c97893a48f7393dc346f",
+    "url": "/watch-it/static/js/2.86213445.chunk.js"
+  },
+  {
+    "revision": "59fc29711a22a223e9f602decc11b79f",
+    "url": "/watch-it/static/js/2.86213445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25715e537e46f857e63a",
+    "url": "/watch-it/static/js/main.5453fa19.chunk.js"
+  },
+  {
+    "revision": "90357619d52982f2f765",
+    "url": "/watch-it/static/js/runtime-main.f5eb5bb5.js"
+  }
+]);
